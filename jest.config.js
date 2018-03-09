4776b763c7ca49890c9c097c7e83bf8f415e2945
Joshua <joshua@frontlawn.net>
2018-03-08 17:20:23 -0800

    misc(tests): Add coverage thresholds.

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,13 @@ module.exports = {
   coveragePathIgnorePatterns: [
     '<rootDir>/dist',
     '/node_modules/'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
 };
